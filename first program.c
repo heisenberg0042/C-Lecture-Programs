17d596ad2521a2c2382ff9cc73cee9b2b9df9f76
heisenberg0042 <94596495+heisenberg0042@users.noreply.github.com>
2021-11-18 18:15:32 +0530

    Add files via upload

---
+++ first program.c
@@ -0,0 +1,42 @@
+// header files   // // pre processor
+
+/*
+
+this
+is
+multi line 
+comment
+
+*/
+
+#include<stdio.h>
+
+// means single line comment
+
+main(){   // compiler starts program at main function
+	
+	printf("Hello World");	  // to print something on console
+	
+	// data type
+	
+	// short int   // -127   // 128   // 1 byte   // 1 byte = 8 bit  // 10101010
+	
+	// int 
+	
+	short int s = 255;
+	short int s1 = 155;
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
