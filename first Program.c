82982e7f7cd792eb09d2a37406387b1a6eb6b583
heisenberg0042 <94596495+heisenberg0042@users.noreply.github.com>
2021-11-20 18:28:53 +0530

    Add files via upload

---
+++ first Program.c
@@ -0,0 +1,70 @@
+// header files
+
+// pre processor
+
+#include<stdio.h>
+
+// function
+
+// block code
+
+main(){
+	
+	printf("Hello Student");	// to print something on screen
+	
+	// data type
+	
+	// 343434 // 5345345.3454355 // f e // "tops"  // % & * (   
+	
+	// short int // -127 -- 128   / 1 byte - 8 bit  // 10101010101
+	
+	short int stdio = 120 ;   // identifier   // variable 
+	
+	// datatype // variable name(indentifier name) = value;
+	
+	// keyword  // reserver words
+	
+	
+	//int // 4 byte  // 32 bit  // -32768 -- 32767  // 
+	
+	int number = 12345;
+	
+	// long int // 4 byte  // 32 bit
+	
+	long int li = 232323;
+	
+	//long long int 8 byte  // 64 bit
+	
+	// 1 kb = 1024 byte
+	
+	// 1 mb = 1024 kb
+	
+	long long int lli = 1010101010;
+	
+	// float// 4 byte // 32 bits
+	
+	float f = 23434.3434;
+	
+	// double 8 byte  // 64 bit
+	
+	double don  = 34234.434343;
+	
+	// long double  // 16 byte  // 128 bit
+	
+	long double badaDon = 324234234234234.3242434;
+	
+	//  % ^ & " ' !    // 1 byte // 8 bit
+	
+	char ch = '';
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
---
+++ printVariables.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+main(){
+	
+	short int si = 120;
+	
+	printf("%u",si);   // format specifier
+	
+	
+	int i = 23;
+	
+	printf("%d",i);
+	
+	
+	
+	long int li = 343434;
+	
+	printf("%ld",li);
+	
+	
+	
+	long long int lli = 34242423234;
+	
+	printf("%lld",lli);
+	
+	
+	
+	float f = 2323.2323;
+	
+	printf("%.3f",f);
+	
+	
+	
+	double d = 3234324.2344;
+	
+	printf("%.4lf",d);
+	
+	
+
+	long double ld = 35345345.34534;
+	
+	printf("%Lf",ld);
+	
+	
+
+	char ch = 'f';
+	
+	printf("%c",ch);
+	
+}
